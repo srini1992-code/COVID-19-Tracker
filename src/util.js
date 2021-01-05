1e7e7d1342f4cde4375907dfeaa1d72533ef6d64
srini1992-code <srinidhis1992dec@gmail.com>
2021-01-05 15:08:56 +0530

    updated

--- src/util.js
+++ src/util.js
@@ -39,20 +39,20 @@ export const showDataOnMap = (data, casesType = 'cases') =>
       }
     >
       <Popup>
-        <div className="info--container">
+        <div className="info-container">
           <div
-            className="info--flag"
+            className="info-flag"
             style={{ backgroundImage: `url(${country.countryInfo.flag})` }}
-          ></div>
-          <div className="info--name">{country.country}</div>
-          <div className="info--confirmed">
-            Cases:{numeral(country.cases).format('0,0')}
+          />
+          <div className="info-name">{country.country}</div>
+          <div className="info-confirmed">
+            Cases: {numeral(country.cases).format('0,0')}
           </div>
-          <div className="info--recovered">
-            Recovered:{numeral(country.recovered).format('0,0')}
+          <div className="info-recovered">
+            Recovered: {numeral(country.recovered).format('0,0')}
           </div>
-          <div className="info--deaths">
-            Deaths:{numeral(country.deaths).format('0,0')}
+          <div className="info-deaths">
+            Deaths: {numeral(country.deaths).format('0,0')}
           </div>
         </div>
       </Popup>
