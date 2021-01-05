5e02eca66dd3c9c248ac9519c78f309b73f1a434
srini1992-code <srinidhis1992dec@gmail.com>
2021-01-05 13:36:42 +0530

    added flag to popup

--- src/Map.js
+++ src/Map.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Map.css';
-import { Map as LeafletMap, TileLayer } from 'react-leaflet';
+import { MapContainer as LeafletMap, TileLayer } from 'react-leaflet';
 import { showDataOnMap } from './util';
 
 function Map({ countries, casesType, center, zoom }) {
@@ -11,7 +11,7 @@ function Map({ countries, casesType, center, zoom }) {
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         />
-        {/* loop through */}
+
         {showDataOnMap(countries, casesType)}
       </LeafletMap>
     </div>
